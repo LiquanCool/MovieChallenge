c8af7945cfec6d0a7418df51977ea7dcabbe37ff
liquancool <llei0111@bths.edu>
2023-02-17 16:01:46 -0500

    Did one degree

--- src/Main.java
+++ src/Main.java
@@ -5,65 +5,38 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<SimpleMovie> movies = MovieDatabaseBuilder.getMovieDB("src/KevinBaconActorMovies.txt");
-        for (SimpleMovie movie : movies) {
-            System.out.println(movie);
-        }
         ArrayList<String> actors1 = MovieDatabaseBuilder.getBaconActors("src/KevinBaconActors.txt");
-        for (String actor : actors1) {
-            System.out.println(actor);
-        }
         System.out.println("Number of movies: " + movies.size());
         Scanner n = new Scanner(System.in);
         System.out.println("What actor do you want to link?");
         String searchTerm = n.nextLine();
         ArrayList<String> results = new ArrayList<String>();
-        results.add("Kevin Bacon");
+        results.add(searchTerm);
         int min = 0;
         int max = movies.size();
         ArrayList<SimpleMovie> tempMovies = new ArrayList<SimpleMovie>();
         tempMovies = movies;
-        /**while (min <= max)
-        {
-            int middle = (min + max) / 2;
-            if (tempMovies.subList(0,middle).contains(searchTerm))
-            {
-                max = middle - 1;
-                movies = tempMovies;
-                movies.clear();
-                System.out.println(movies.size());
-            }
-            else if (tempMovies.subList(middle,tempMovies.size()-1).contains(searchTerm))
-            {
-                min = middle + 1;
-                movies = tempMovies;
-                movies.clear();
-            }
-            else
-            {
-                results.add(movies.get(middle).getTitle());
-                System.out.println(movies.size());
-                System.out.println(tempMovies.size());
-
-            }
-        }
-**/
-
-        /**for (int i =0;i<movies.size();i++)
+        for (int i =0;i<movies.size()&&results.size()<2;i++)
         {
             ArrayList<String> list1 = movies.get(i).getActors();
-            for (int j = 0;j<list1.size();i++)
+            if (list1.contains(searchTerm))
             {
+                results.add(movies.get(i).getTitle());
+            }
+            /**for (int j = 0;j<list1.size();i++)
+            {
+                System.out.println("yes");
+                System.out.println(results.size());
                 if (list1.get(j).equals(searchTerm))
                 {
                     results.add(movies.get(i).getTitle());
                 }
-            }
+             }**/
         }
-        **/
-        results.add(searchTerm);
         for (int i = 0;i<results.size();i++)
         {
             System.out.print(results.get(i)+ " --> ");
         }
+        System.out.print("Kevin Bacon");
     }
 }
